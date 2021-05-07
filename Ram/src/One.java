2e4b51145f6dddca1fc568c464cd26fd453ecbb5
Ezekiel Ramirez <ezekiel.ramirez@calbaptist.edu>
2021-05-06 20:49:33 -0700

    Comment here

---
+++ Ram/src/One.java
@@ -0,0 +1,52 @@
+/**
+ * Hello I am Ezekiel Ramirez
+ *
+ * I am a computer science major at California Baptist University. I have some friends who
+ * are changing majors to comp sci and I am creating these repos to help them learn basic
+ * syntax in the java language. Most program languages behave the same, the major
+ * difference being that the syntax changes.
+ *
+ * In this first lesson we will cover
+ * - variable types
+ * - outputting information to the screen
+ * - using variables to create functions
+ *
+ * For more examples of what I cover in these programs you can head to w3schools java lessons
+ * If you find yourself on a computer that doesn't have a programing environment set up
+ * you can look up any online IDE, preferably one that has java.
+ *
+ * GIT commands
+ * git add --all
+ * git commit -m "Comment here"
+ * git push
+ */
+
+public class One {
+
+    public static void main(String[] args) {
+        // to make comments in most java IDE's you use two slashes
+
+        /* for comments
+            that span multiple lines use the multi line comment
+         */
+
+        /**
+         * For special notes that would be important to users
+         * use this style of commenting!!!
+         */
+
+        int integer = 0;
+
+        double number = 00;
+
+        float weAll = 0;
+
+        String str = "hello";
+
+        char abc = 'd';
+
+        boolean either = false;
+
+        System.out.println("hello, world!");
+    }
+}
